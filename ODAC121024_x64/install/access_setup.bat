820e8e71a522f2529e7555676868a8147576f2ec
Marcos Yang <marcos.yang@fairhr.com>
2017-08-07 08:52:51 +0800

    删除软件

--- ODAC121024_x64/install/access_setup.bat
+++
@@ -1,5 +0,0 @@
-echo Copying Accessibility related files 
-copy access\jdk\jre\bin\WindowsAccessBridge-64.dll %SystemRoot%\System32\WindowsAccessBridge-64.dll
-@echo Copy complete
-@echo Restart Jaws before starting an install session
-
